891d2b8198e5f8b9ce2651726f2075fbf8eeee94
Navin82005 <navin82005@gmail.com>
2023-12-03 06:57:39 +0530

    updated appconfig

--- lib/themes/AppConfig.dart
+++ lib/themes/AppConfig.dart
@@ -2,5 +2,6 @@
 
 class AppConfig {
   static const appName = "EduMatricsPro";
-  static const backendUrl = "http://127.0.0.1:8000";
+  // static const backendUrl = "http://127.0.0.1:8000";
+  static const backendUrl = "https://navin82005.pythonanywhere.com/";
 }
